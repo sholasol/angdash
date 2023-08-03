d1980abd433644fa1c13667fa4c1b3b209af74b7
Solomon <sholasolomon@yahoo.com>
2023-08-03 23:45:46 +0100

    Modify style

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter,HostListener,OnInit,Output } from '@angular/core';
 import { navbarData } from './nav-data';
+import { animate, animation, keyframes, style, transition, trigger } from '@angular/animations';
 
 interface SideNavToggle {
   screenWidth : number;
@@ -9,7 +10,32 @@ interface SideNavToggle {
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity: 0}),
+        animate('350ms', 
+          style({opacity: 1})
+        )
+      ]),
+      transition(':leave', [
+        style({opacity: 1}),
+        animate('350ms', 
+          style({opacity: 0})
+        )
+      ])
+    ]),
+    trigger('rotate', [
+      transition(':enter', [
+        animate('1000ms', 
+        keyframes([
+          style({trasform: 'rotate(0deg)', offset: '0'}),
+          style({trasform: 'rotate(2turn)', offset: '1'})
+        ]))
+      ])
+    ])
+  ]
 })
 
 export class SidenavComponent implements OnInit{
